d009db4584fdd8fdc382e5d1b914b1419ac0c2b3
Raveeha Mohsin <mohsinraveeha@gmail.com>
2023-05-14 20:28:22 +0500

    message

---
+++ week 5 oop (Business Application) with (BL+ DL + UI)/week 5 oop (Business Application) with (BL+ DL + UI)/Muser CRUD/MuserCRUD.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week_5_oop__Business_Application__with__BL__DL___UI_.Muser_BL;
+
+namespace week_5_oop__Business_Application__with__BL__DL___UI_.Muser_CRUD
+{
+    public static class MuserCRUD
+    {
+        public static void addtolist(Muser m, List<Muser> flightslists)
+        {
+            flightslists.Add(m);
+        }
+
+
+        public static void viewflights(List<Muser> flights)
+        {
+            for (int idx = 0; idx < flights.Count; idx++)
+            {
+                Console.WriteLine("Your flight Schedule is from {0} to {1}", flights[idx].currentlocation, flights[idx].desiredlocation);
+
+            }
+
+        }
+
+        public static void updateflights(List<Muser> flights)
+        {
+            int index;
+            Console.WriteLine("Enter the index of flight you want to change");
+            index = int.Parse(Console.ReadLine());
+            index = index - 1;
+            Console.WriteLine("Enter your new current location");
+            flights[index].currentlocation = Console.ReadLine();
+            Console.WriteLine("Enter your new desired location");
+            flights[index].desiredlocation = Console.ReadLine();
+
+            
+        }
+
+        public  static void deleterecord(List<Muser> flights)
+        {
+            int index;
+            Console.WriteLine("Enter the index of flight you want to delete");
+            index = int.Parse(Console.ReadLine());
+            index = index - 1;
+            flights.RemoveAt(index);
+        }
+    }
+}
+
+
+  
---
+++ week 5 oop (Business Application) with (BL+ DL + UI)/week 5 oop (Business Application) with (BL+ DL + UI)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week_5_oop__Business_Application__with__BL__DL___UI_.menu_UI;
+using week_5_oop__Business_Application__with__BL__DL___UI_.Muser_BL;
+using week_5_oop__Business_Application__with__BL__DL___UI_.Muser_CRUD;
+using week_5_oop__Business_Application__with__BL__DL___UI_.Muser_UI;
+
+namespace week_5_oop__Business_Application__with__BL__DL___UI_
+{
+    public class Program
+    {
+        static List<Muser> flights = new List<Muser>();
+        static void Main(string[] args)
+        {
+            int option;
+            do
+            {             
+                Console.Clear();
+                option = menuUI.optmenu();
+                Console.Clear();
+                if (option == 1)
+                {
+                    Muser user = muserUI.takeinput();
+                    MuserCRUD.addtolist(user, flights);
+
+                }
+                else if (option == 2)
+                {
+                    MuserCRUD.viewflights(flights);
+                    Console.ReadKey();
+                }
+                else if (option == 3)
+                {
+                    MuserCRUD.updateflights(flights);
+
+                }
+                else if (option == 4)
+                {
+                    MuserCRUD.deleterecord(flights);
+                }
+                else if (option == 5)
+                {
+                    break;
+                }
+
+            }
+            while (option < 6);
+            Console.Read();
+
+            Console.ReadKey();
+        }
+    }
+ }
